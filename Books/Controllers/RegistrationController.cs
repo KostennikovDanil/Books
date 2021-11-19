93a7fe601142291941c6c67cfda08c84c7a9a7d1
KostennikovDanil <55431913+KostennikovDanil@users.noreply.github.com>
2021-11-19 07:46:32 +0300

    Initial 2

---
+++ Books/Controllers/RegistrationController.cs
@@ -0,0 +1,43 @@
+﻿using BooksService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace Books.Controllers
+{
+    [Route("api/v1/user")]
+    [ApiController]
+    public class RegistrationController
+    {
+
+        [Route("/api/v1/user/register")]
+        [HttpPost()]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Produces(MediaTypeNames.Application.Json)]
+        [Consumes(MediaTypeNames.Application.Json)]
+        public async Task<ActionResult<UserRegistrationResponse>> RegisterAsync([FromBody] UserRegistrationRequest request)
+        {
+            //return await RestMethod(request,
+            //    _ => userValidator.Validate(_),
+            //    async _ =>
+            //    {
+            //        var result = await userService.RegisterAsync(_, CancellationToken.None);
+
+            //        if (result.ResponseCode != UserRegistrationResponse.RegistrationResponseCode.Success)
+            //            return result;
+
+            //        await userService.SaveAllChangesAsync();
+
+            //        return result;
+            //    });
+
+
+        }
+
+    }
+}
